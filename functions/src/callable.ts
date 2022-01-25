2c16c0ab0420d02f753141a8bb45f876e7777a0d
Pete VS <pete_vs88@me.com>
2022-01-25 12:47:48 -0700

    parsing and summarizing peers and basic values setup

--- functions/src/callable.ts
+++ functions/src/callable.ts
@@ -27,67 +27,107 @@ const summary = (data: any) => {
       shakepayBitcoin: 0,
       coldStorage: 0, 
     }
+
+    const aggregates = {
+        totalBitcoinPurchased: 0,
+        totalSpent: 0,
+        totalCardSpend: 0,
+        totalShakingSats: 0,
+        totalCashBackBTC: 0,
+    }
+
+    const peers: any = {}
   
     data.forEach((transaction: { [x: string]: any }) => {
 
-        const debitCurrency = transaction['Debit Currency']
+        // const debitCurrency = transaction['Debit Currency']
         const debitAmount = Number(transaction['Amount Debited'])
-        const creditCurrency = transaction['Credit Currency']
+        // const creditCurrency = transaction['Credit Currency']
         const creditAmount = Number(transaction['Amount Credited'])
         const transactionType = transaction['Transaction Type']
         const direction = transaction['Direction']
-        const rate = Number(transaction['Buy / Sell Rate'])
-        const spotRate = Number(transaction['Source / Direction'])
-
+        // const rate = Number(transaction['Buy / Sell Rate'])
+        // const spotRate = Number(transaction['Source / Direction'])
+        const sourceDestination = transaction['Source / Destination']
   
-      switch(transactionType){
-          case ('fiat funding'):
-              wallets.shakepayDollars += creditAmount
-              return
-          case ('fiat cashout'):
-                wallets.shakepayDollars -= debitAmount
-              return
-          case ('other'):
-                wallets.shakepayDollars += (-1 * debitAmount) + creditAmount
-              return
-          case ('card transactions'):
-                wallets.shakepayDollars -= debitAmount
-              return
-          case ('purchase/sale'):
-            if(direction === 'purchase') {
-                wallets.shakepayDollars -= debitAmount
-                wallets.shakepayBitcoin += creditAmount
-            }
-            else {
+        switch(transactionType){
+            case ('fiat funding'):
                 wallets.shakepayDollars += creditAmount
+                return
+            case ('fiat cashout'):
+                  wallets.shakepayDollars -= debitAmount
+                return
+            case ('other'):
+                  wallets.shakepayDollars += (-1 * debitAmount) + creditAmount
+                return
+            case ('card transactions'):
+                  wallets.shakepayDollars -= debitAmount
+                  aggregates.totalCardSpend += debitAmount
+                return
+            case ('purchase/sale'):
+              if(direction === 'purchase') {
+                  wallets.shakepayDollars -= debitAmount
+                  aggregates.totalSpent += debitAmount
+                  wallets.shakepayBitcoin += creditAmount
+                  aggregates.totalBitcoinPurchased += creditAmount
+                  
+              }
+              else {
+                  wallets.shakepayDollars += creditAmount
+                  wallets.shakepayBitcoin -= debitAmount
+              }
+              return
+            case ('crypto funding'):
+                wallets.shakepayBitcoin += creditAmount
+                wallets.coldStorage -= creditAmount
+                return
+            case ('crypto cashout'):
                 wallets.shakepayBitcoin -= debitAmount
-            }
-            return
-          case ('crypto funding'):
+                wallets.coldStorage += debitAmount
+                return
+            case ('peer transfer'):
+              wallets.shakepayDollars += -debitAmount + creditAmount
+              // create peer if does not exist
+              if(!(sourceDestination in peers)) {
+                peers[sourceDestination] = {
+                  sent: 0,
+                  received: 0,
+                  net: 0
+                }
+              }
+              
+              peers[sourceDestination].sent += debitAmount || 0
+              peers[sourceDestination].received += creditAmount || 0
+              peers[sourceDestination].net += creditAmount - debitAmount
+                return
+            case ('shakingsats'):
               wallets.shakepayBitcoin += creditAmount
-              wallets.coldStorage -= creditAmount
-              return
-          case ('crypto cashout'):
-              wallets.shakepayBitcoin -= debitAmount
-              wallets.coldStorage += debitAmount
-              return
-          case ('peer transfer'):
-            wallets.shakepayDollars += -debitAmount + creditAmount
-              return
-          case ('shakingsats'):
-            wallets.shakepayBitcoin += creditAmount
-              return
-          case ('card cashbacks'):
-            wallets.shakepayBitcoin += creditAmount
-              return
-          default:
-              return
+              aggregates.totalShakingSats += creditAmount
+                return
+            case ('card cashbacks'):
+              wallets.shakepayBitcoin += creditAmount
+              aggregates.totalCashBackBTC += creditAmount
+                return
+            default:
+                return
+        }
+    
+      })
+  
+  
+    return {
+        wallets: {
+          ...wallets
+        },
+        aggregates: {
+          ...aggregates,
+          totalBitcoinEarned: aggregates.totalShakingSats + aggregates.totalCashBackBTC,
+          averagePurchasePrice: aggregates.totalSpent / aggregates.totalBitcoinPurchased
+        },
+        peers: {
+          ...peers
+        }
       }
-  
-    })
-  
-  
-    return wallets
   
   }
   
@@ -105,6 +145,6 @@ export const parseShakepay = functions.https.onCall(async (req, context) => {
 
     const transactions = res.data
 
-    return transactions
+    return summary(transactions)
 
 })
